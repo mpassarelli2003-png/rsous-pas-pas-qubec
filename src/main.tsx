7086f09483268a2d4f6f7e04af2e48cb5e49cd88
mpassarelli2003-png <mpassarelli2003@gmail.com>
2026-05-18 11:18:06 -0400

    Retirer les providers Blink obligatoires

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BlinkUIProvider, Toaster } from '@blinkdotnew/ui'
-import { BlinkProvider, BlinkAuthProvider } from '@blinkdotnew/react'
 import App from './App'
 import './index.css'
 
@@ -10,17 +9,13 @@ const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BlinkProvider projectId={import.meta.env.VITE_BLINK_PROJECT_ID}>
-      <BlinkAuthProvider>
-        <QueryClientProvider client={queryClient}>
-          <BlinkUIProvider theme="minimal" darkMode="system">
-            <Toaster />
-            <div className="flex w-full flex-1 flex-col min-h-0">
-              <App />
-            </div>
-          </BlinkUIProvider>
-        </QueryClientProvider>
-      </BlinkAuthProvider>
-    </BlinkProvider>
+    <QueryClientProvider client={queryClient}>
+      <BlinkUIProvider theme="minimal" darkMode="system">
+        <Toaster />
+        <div className="flex w-full flex-1 flex-col min-h-0">
+          <App />
+        </div>
+      </BlinkUIProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
