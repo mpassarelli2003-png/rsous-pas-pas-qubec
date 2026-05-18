5ebd5ee02d87573f60a329be48314ffe3968a5ad
mpassarelli2003-png <mpassarelli2003@gmail.com>
2026-05-18 11:33:11 -0400

    Migrer main vers le module UI local

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { BlinkUIProvider, Toaster } from '@blinkdotnew/ui'
+import { BlinkUIProvider, Toaster } from '@/lib/ui'
 import App from './App'
 import './index.css'
 
